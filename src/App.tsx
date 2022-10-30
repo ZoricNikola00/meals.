1d5e058ccea8be2645ffdd449f3e71bb25161286
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-30 20:23:18 +0100

    Creating Specific Category page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import List from './pages/List';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import SingleDish from './pages/SingleDish';
+import SpecificCategory from './pages/SpecificCategory';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/list/:str' element={<List/>}/>
         <Route path='/dish/:id' element={<SingleDish/>}/>
+        <Route path='/category/:str' element={<SpecificCategory/>}/>
       </Routes>
     </>
   );
