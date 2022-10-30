94bae0a7528c61c88267dd0b34443e530dc1e50b
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-30 20:22:54 +0100

    Minor edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import AllCategories from '../components/AllCategories'
 const Home = () => {
   return (
     <>
-        <div className='w-[90%] md:w-[85%] lg:w-[75%] mx-auto shadow-xl p-4 rounded-lg my-4'>
+        <div className='w-[90%] md:w-[85%] lg:w-[75%] mx-auto shadow-xl p-4 rounded-lg my-20'>
             <h1 className='text-center font-bold text-6xl'>Welcome To Meals.</h1>
         </div>
         <AllCategories/>
