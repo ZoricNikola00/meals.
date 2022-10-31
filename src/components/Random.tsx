ee333eb620dd0cb4c432464124d6d4012db489f4
ZoricNikola00 <zoriclfc@gmail.com>
2022-11-01 00:19:15 +0100

    Adding two meals in random page

--- src/components/Random.tsx
+++ src/components/Random.tsx
@@ -15,22 +15,37 @@ type Data={
     return data?.data?.meals[0]
 }
 const Random = () => {
-    const {data,isLoading,refetch}=useQuery(['random'],fetchRandom)
+    const {data:meal1,isLoading,refetch:r1}=useQuery(['random',1],fetchRandom,{refetchOnWindowFocus:false})
+    const {data:meal2,refetch:r2}=useQuery(['random',2],fetchRandom,{refetchOnWindowFocus:false})
+
+    
    
     useEffect(()=>{
-        const time=setTimeout(()=>{refetch()},5000)
+        const time=setTimeout(()=>{r1();r2()},5000)
         return ()=>clearTimeout(time)
-    },[data])
-   
+    },[meal1])
+    if(isLoading){
+        return <div>LOADING</div>
+    }
   return (
-    <div className='w-[90%] md:w-[85%] lg:w-[55%] mx-auto shadow-xl p-4 rounded-lg my-4'>
-        <h1 className='text-2xl'>Try This Specialty</h1>
-        <div className='w-full flex my-10 flex-col md:flex-row'>
-            <Link to={`/dish/${data?.idMeal}`}><img className='w-[300px] mx-auto md:w-[400px] rounded-xl' src={data?.strMealThumb}/></Link>
-            <div className='ml-4 my-4 md:my-0'>
-                <h1 className='text-2xl md:text-4xl'>{data?.strMeal}</h1>
-                <p className='text-gray-600 text-lg my-8'>Category: {data?.strCategory}</p>
-                <p className='text-gray-600 text-lg my-8'>Origin: {data?.strArea}</p>
+    <div className='w-full mx-auto my-20'>
+        <h1 className='text-2xl'>Try One Of These Specialties :</h1>
+        <div className='flex '>
+            <div className='w-[50%] flex mt-10 flex-col md:flex-row'>
+                <Link to={`/dish/${meal1?.idMeal}`} className='w-[90%] md:w-[60%]'><img className='w-full object-cover rounded-xl' src={meal1?.strMealThumb}/></Link>
+                <div className='md:ml-4 my-4 md:my-0 w-[90%] md:w-[40%]'>
+                    <h1 className='text-lg md:text-2xl'>{meal1?.strMeal}</h1>
+                    <p className='text-gray-600 text-lg my-8'>Category: {meal1?.strCategory}</p>
+                    <p className='text-gray-600 text-lg my-8'>Origin: {meal1?.strArea}</p>
+                </div>
+            </div>
+            <div className='w-[50%] flex mt-10 flex-col md:flex-row'>
+                <Link to={`/dish/${meal2?.idMeal}`} className='w-[90%] md:w-[60%]'><img className='w-full object-cover rounded-xl' src={meal2?.strMealThumb}/></Link>
+                <div className='md:ml-4 my-4 md:my-0 w-[90%] md:w-[40%]'>
+                    <h1 className='text-lg md:text-2xl '>{meal2?.strMeal}</h1>
+                    <p className='text-gray-600 text-lg my-8'>Category: {meal2?.strCategory}</p>
+                    <p className='text-gray-600 text-lg my-8'>Origin: {meal2?.strArea}</p>
+                </div>
             </div>
         </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,8 +9,8 @@ const Home = () => {
     <>
         <div className='w-[90%] md:w-[85%] lg:w-[75%] mx-auto shadow-xl p-4 rounded-lg my-20'>
             <h1 className='text-center font-bold text-6xl'>Welcome To Meals.</h1>
+            <Random/>
         </div>
-        <Random/>
         <AllCategories/>
         <AllAreas/>
     </>
