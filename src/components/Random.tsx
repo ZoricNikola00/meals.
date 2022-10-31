f6a4bb9842eddda7574f67f43523c615f1e3a654
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-31 23:13:23 +0100

    Creating component for random meals on Home page

---
+++ src/components/Random.tsx
@@ -0,0 +1,40 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+type Data={
+    idMeal:number,
+    strCategory:string,
+    strMealThumb:string,
+    strArea:string,
+    strCategoryDescription:string
+  }
+
+  const fetchRandom=async()=>{
+    const data=await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+    return data?.data?.meals[0]
+}
+const Random = () => {
+    const {data,isLoading,refetch}=useQuery(['random'],fetchRandom)
+   
+    useEffect(()=>{
+        const time=setTimeout(()=>{refetch()},5000)
+        return ()=>clearTimeout(time)
+    },[data])
+   
+  return (
+    <div className='w-[90%] md:w-[85%] lg:w-[55%] mx-auto shadow-xl p-4 rounded-lg my-4'>
+        <h1 className='text-2xl'>Try This Specialty</h1>
+        <div className='w-full flex my-10 flex-col md:flex-row'>
+            <Link to={`/dish/${data?.idMeal}`}><img className='w-[300px] md:w-[400px] rounded-xl' src={data?.strMealThumb}/></Link>
+            <div className='ml-4'>
+                <h1 className='text-2xl md:text-4xl'>{data?.strMeal}</h1>
+                <p className='text-gray-600 text-lg my-8'>Category: {data?.strCategory}</p>
+                <p className='text-gray-600 text-lg my-8'>Origin: {data?.strArea}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Random
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 
 import AllAreas from '../components/AllAreas'
 import AllCategories from '../components/AllCategories'
+import Random from '../components/Random'
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
         <div className='w-[90%] md:w-[85%] lg:w-[75%] mx-auto shadow-xl p-4 rounded-lg my-20'>
             <h1 className='text-center font-bold text-6xl'>Welcome To Meals.</h1>
         </div>
+        <Random/>
         <AllCategories/>
         <AllAreas/>
     </>
