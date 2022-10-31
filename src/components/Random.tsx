07b9c5e352f94dd324df48c7f55b7c5c5b83e823
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-31 23:31:37 +0100

    Small change

--- src/components/Random.tsx
+++ src/components/Random.tsx
@@ -26,8 +26,8 @@ const Random = () => {
     <div className='w-[90%] md:w-[85%] lg:w-[55%] mx-auto shadow-xl p-4 rounded-lg my-4'>
         <h1 className='text-2xl'>Try This Specialty</h1>
         <div className='w-full flex my-10 flex-col md:flex-row'>
-            <Link to={`/dish/${data?.idMeal}`}><img className='w-[300px] md:w-[400px] rounded-xl' src={data?.strMealThumb}/></Link>
-            <div className='ml-4'>
+            <Link to={`/dish/${data?.idMeal}`}><img className='w-[300px] mx-auto md:w-[400px] rounded-xl' src={data?.strMealThumb}/></Link>
+            <div className='ml-4 my-4 md:my-0'>
                 <h1 className='text-2xl md:text-4xl'>{data?.strMeal}</h1>
                 <p className='text-gray-600 text-lg my-8'>Category: {data?.strCategory}</p>
                 <p className='text-gray-600 text-lg my-8'>Origin: {data?.strArea}</p>
