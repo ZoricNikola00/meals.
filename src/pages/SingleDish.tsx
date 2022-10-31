842bfc7d12d6eb399ace1be7153e0c6dd5350122
ZoricNikola00 <zoriclfc@gmail.com>
2022-11-01 00:12:00 +0100

    Add navigate to singleDish page

--- src/pages/SingleDish.tsx
+++ src/pages/SingleDish.tsx
@@ -1,4 +1,4 @@
-import {useParams} from 'react-router-dom'
+import {useParams,useNavigate} from 'react-router-dom'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
 import { useState, } from 'react'
@@ -17,7 +17,7 @@ const SingleDish = () => {
     const {id}=useParams()
     const [sliced,setSliced]=useState(true)
     const [liked,setLiked]=useState(false)
-
+    const navigate=useNavigate()
     const {data}=useQuery(['meal',id],()=>fetchMeal(id),{onSuccess:()=>{
       if(user?.email){
       onSnapshot(doc(db,'users',`${user?.email}`),(d)=>{
@@ -66,7 +66,7 @@ const SingleDish = () => {
                 <h1 className='text-xl uppercase '>{data?.strMeal}</h1>
                 <button onClick={saveMeal} className='text-gray-600 text-5xl cursor-pointer' >{liked?<AiFillHeart />:<AiOutlineHeart/>}</button>
               </div>
-              <div className='flex mb-4'><p className='text-gray-500'>Category: <span className=''>{data?.strCategory}</span></p> <p className='ml-5 text-gray-500'>Origin: <span>{data?.strArea}</span></p></div>
+              <div className='flex mb-4'><p className='text-gray-500'>Category: <span className='cursor-pointer hover:text-gray-700' onClick={_=>navigate(`/category/c/${data?.strCategory}`)}>{data?.strCategory}</span></p> <p className='ml-5 text-gray-500'>Origin: <span className='cursor-pointer hover:text-gray-700' onClick={_=>navigate(`/category/a/${data?.strArea}`)}>{data?.strArea}</span></p></div>
               <div className='w-[100%] md:w-[80%] flex flex-col'>
                 <h2 className='mb-2'>Instructions:</h2>
                 <p className='mb-2 md:p-4 whitespace-pre-line md:shadow-xl md:rounded-xl'>{slicedText(data?.strInstructions,500)} {data?.strInstructions.length>500 && <button className='text-gray-500 cursor-pointer' onClick={_=>setSliced((p:boolean)=>!p)}>{sliced?'Read More':'Show Less'}</button>}</p>
