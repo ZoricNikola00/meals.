ab5cbb87a67507a7847a2ab2038f9c44388feebc
ZoricNikola00 <zoriclfc@gmail.com>
2022-10-31 16:30:12 +0100

    Adding function for adding meals to account

--- src/pages/SingleDish.tsx
+++ src/pages/SingleDish.tsx
@@ -1,7 +1,12 @@
 import {useParams} from 'react-router-dom'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {AiFillHeart,AiOutlineHeart} from 'react-icons/ai'
+import { useGlobalContext } from '../context'
+import { updateDoc,doc, arrayUnion, getDoc, collection, getDocs } from 'firebase/firestore'
+import { db } from '../firebase'
+import { async } from '@firebase/util'
 
 const fetchMeal=async(id:string|undefined)=>{
     const data=await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
@@ -17,9 +22,22 @@ type Data={
     strArea:string,
   }
 const SingleDish = () => {
+    const {user}=useGlobalContext()
     const {id}=useParams()
     const [sliced,setSliced]=useState(true)
-    const {data,isLoading}=useQuery(['meal',id],()=>fetchMeal(id))
+    const [liked,setLiked]=useState(false)
+    const [like,setLike]=useState(false)
+    const {data,isLoading}=useQuery(['meal',id],()=>fetchMeal(id),{onSuccess:()=>{
+      const check=async()=>{
+        const ref=collection(db,'users')
+        const dat=await getDocs(ref);
+        dat.docs.map((x)=>{
+          if(x.data().favoriteMeals.some((y:any)=>y.idMeal===data?.idMeal)){
+            return setLiked(true)
+          }
+        })}
+        check()
+    }})
     //const {strMeal,strArea,strMealThumb,strCategory,strInstructions}=data
     const slicedText=(str:string,num:number)=>{
         if(str?.length > num && sliced){
@@ -27,12 +45,30 @@ const SingleDish = () => {
         }
         return str
     }
+    const mealId=doc(db,'users',`${user?.email}`)
+    const saveMeal=async()=>{
+      if(user?.email){
+        setLiked(true)
+        await updateDoc(mealId,{
+          favoriteMeals:arrayUnion({
+            ...data
+          })
+        })
+      }
+      else{
+        alert("You are not logged in!")
+      }
+    }
+   
   return (
     <>
       <div className='mt-4 w-[90%] p-4 md:w-[85%] lg:w-[75%] flex flex-col md:flex-row rounded-lg shadow-xl mx-auto'>
           <img src={data?.strMealThumb} className='md:hidden block w-[100%] h-fit rounded-xl'/>
           <div className='w-[100%] md:w-[60%] p-2 md:p-0'>
-              <h1 className='text-xl uppercase mb-2'>{data?.strMeal}</h1>
+              <div className='mb-2 flex items-center justify-between   w-[100%] md:w-[80%]'>
+                <h1 className='text-xl uppercase '>{data?.strMeal}</h1>
+                <button onClick={saveMeal} className='text-gray-600 text-5xl cursor-pointer' >{liked?<AiFillHeart />:<AiOutlineHeart/>}</button>
+              </div>
               <div className='flex mb-4'><p className='text-gray-500'>Category: <span className=''>{data?.strCategory}</span></p> <p className='ml-5 text-gray-500'>Origin: <span>{data?.strArea}</span></p></div>
               <div className='w-[100%] md:w-[80%] flex flex-col'>
                 <h2 className='mb-2'>Instructions:</h2>
